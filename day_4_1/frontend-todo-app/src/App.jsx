62a886154e779788b21734eaa81312246adb0202
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-02-08 23:39:51 +0530

    pratice done

--- day_4_1/frontend-todo-app/src/App.jsx
+++ day_4_1/frontend-todo-app/src/App.jsx
@@ -4,15 +4,55 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const[todo, setTodo] = useState(
     {
       title: "go to the gym",
       desc : "GYM",
       id : 1
+    },
+    {
+      title: "go to the bar",
+      desc : "BAR",
+      id : 2
     }
   );
+ 
+  // let array = [{
+  //   name : "rohan",
+  //   age : 24
+  // },{
+  //   name : "snehal",
+  //   age : 21
+  // },{
+  //   name : "rushee",
+  //   age : 27
+  // }]
 
+  //let newArray = [];
+
+  // function doubleArray(array){
+  //   array.map((e) => {
+  //     if(e.age > 22){
+  //       newArray.push({
+  //         name : e.name,
+  //         age : e.age,
+  //         isAllowed : true
+  //       })
+  //     }else{
+  //       newArray.push({
+  //         name : e.name,
+  //         age : e.age,
+  //         isAllowed : false
+  //       })
+  //     }
+  //   })  
+  //   console.log(newArray+" array is");
+  // }
+  // for(let i = 0 ; i < newArray.length; i++){
+  //   console.log(newArray[i]);
+  // }
+
+  // doubleArray(array);
   // var todos = [{
   //     title: "go to the gym",
   //     desc : "GYM",
@@ -25,33 +65,38 @@ function App() {
   //   }
   // ]
 
-  setInterval(() =>{
-    setTodo({
-      title : "go eat something",
-      desc : "eat food",
-      id : 1
-    }, 2000);
-  })
+//   setInterval(() =>{
+//     setTodo({
+//       title : "go eat something",
+//       desc : "eat food",
+//       id : 1
+//     }, 100);
+//   })
 
 
- setInterval(() =>{
-    todo.title="rohan"
- }, 1000)
+//  setInterval(() =>{
+//     todo.title="rohan"
+//  }, 1000)
  return (
-  
-    <>
-      <h1>Hi There</h1>
-      {todo.title}
-      {todo.desc}
-      {todo.id}
-      <PersonName firstName={"Rohan"} lastName={"Jadhav"}></PersonName>
-    </>
+    <div>
+      
+       <TODO title ={todo.title} desc = {todo.desc}></TODO>
+       <TODO title ={todo.title} desc = {todo.desc}></TODO>
+       <TODO title ={todo.title} desc = {todo.desc}></TODO>
+    </div>
   )
 }
 
-function PersonName(props){
+// function PersonName(props){
+//   return <div>
+//     {props.firstName} {props.lastName}
+//   </div>
+// }
+
+function TODO(props){
   return <div>
-    {props.firstName} {props.lastName}
+    {props.title}
+    {props.desc}
   </div>
 }
 export default App
