aa6cfc99ab608d6d81c87d8145d8d85dbcb6548c
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-02-19 13:02:19 +0530

    added the week5 learning

---
+++ week_5/react_learning_week5/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Signup from './Signup'
+
+function App() {
+
+
+  return (
+    <div style ={{width : '100vw', height:'100vh', backgroundColor:'whitesmoke'}}>
+      <Signup />  
+    </div>
+  )
+}
+
+export default App
---
+++ week_5/react_learning_week5/src/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div style={{marginTop : '100px', marginBottom : '10px', display:'flex', justifyContent:'center'}}> Rohan
+    <br />
+    <button>Sign up</button>
+    </div>
+  )
+}
+
+export default Signup
