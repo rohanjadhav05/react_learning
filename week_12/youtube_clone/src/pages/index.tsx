23637a8b8ae20604d7cb76751212197492155704
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-03-20 23:38:22 +0530

    added the video link

--- week_12/youtube_clone/src/pages/index.tsx
+++ week_12/youtube_clone/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { VideoCard } from "@/components/VideoCard";
 import { VideoGrid } from "@/components/VideoGrid";
 import { AppBar } from "@/components/AppBar";
 import { LeftBar } from "@/components/LeftBar";
